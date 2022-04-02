9bc38aef654af337d1338866f7b2c16b28a283fa
Maciejbb <95124012+Maciejbb@users.noreply.github.com>
2022-04-02 15:45:50 +0200

    commit

--- src/main/java/com/sda/weather/UserInterface/LocationController.java
+++ src/main/java/com/sda/weather/UserInterface/LocationController.java
@@ -1,9 +1,37 @@
-package com.sda.weather.UserInterface;
+package com.sda.weather.location;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 public class LocationController {
-    public String getLocations() {
-    }
+
+    private final ObjectMapper objectMapper;
+    private final LocationService locationService;
+    private final LocationMapper locationMapper;
 
     public String createLocation(String city, String region, String country, String longitude, String latitude) {
+        try {
+            Location location = locationService.createLocation(city, region, country, longitude, latitude);
+            LocationDTO locationDTO = locationMapper.asLocationDTO(location);
+            return objectMapper.writeValueAsString(locationDTO);
+        } catch (Exception e) {
+            return String.format("{\"status\": \"error\", \"message\": \"%s\"}", e.getMessage());
+        }
+    }
+
+    public String getLocations() {
+        try {
+            List<LocationDTO> locations = locationService.getLocations().stream()
+                    .map(locationMapper::asLocationDTO)
+                    .collect(Collectors.toList());
+
+            return objectMapper.writeValueAsString(locations);
+        } catch (Exception e) {
+            return String.format("{\"status\": \"error\", \"message\": \"%s\"}", e.getMessage());
+        }
     }
 }
